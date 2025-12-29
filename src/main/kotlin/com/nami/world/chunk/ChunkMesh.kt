b61341bca08eb0cc15fdcaabd58f59ed9156d5f2
Memphis Herghelegiu <memphispc70@gmail.com>
2025-12-30 00:19:36 +0100

    optimized chunk mesh generation

--- src/main/kotlin/com/nami/world/chunk/ChunkMesh.kt
+++ src/main/kotlin/com/nami/world/chunk/ChunkMesh.kt
@@ -2,9 +2,11 @@ package com.nami.world.chunk
 
 import com.nami.extension.plus
 import com.nami.world.block.Layer
-import org.joml.Vector2f
 import org.joml.Vector3i
+import org.lwjgl.BufferUtils
 import org.lwjgl.opengl.GL33.*
+import java.nio.FloatBuffer
+import java.nio.IntBuffer
 
 class ChunkMesh(
     val chunk: Chunk,
@@ -26,95 +28,114 @@ class ChunkMesh(
         setBufferData(vertices, indices)
     }
 
-    fun generateData(): Pair<FloatArray, IntArray> {
-        val vertices = mutableListOf<Vertex>()
-        val indices = mutableListOf<Int>()
+    fun generateData(): Pair<FloatBuffer, IntBuffer> {
+        val vertexMap = mutableMapOf<Vertex, Int>()
+
+        val verticesArrayList = ArrayList<Float>()
+        val indicesArrayList = ArrayList<Int>()
 
         val filtered = chunk.voxels.filter { (_, voxel) -> voxel.block?.layer == layer }
 
         for ((position, _) in filtered) {
             // Face X Positive
             if (!filtered.containsKey(position + Vector3i(1, 0, 0)))
-                addFace(vertices, indices, position, Face.EAST)
+                addFace(vertexMap, verticesArrayList, indicesArrayList, position, Face.EAST)
 
             // Face X Negative
             if (!filtered.containsKey(position + Vector3i(-1, 0, 0)))
-                addFace(vertices, indices, position, Face.WEST)
+                addFace(vertexMap, verticesArrayList, indicesArrayList, position, Face.WEST)
 
             // Face Y Positive
             if (!filtered.containsKey(position + Vector3i(0, 1, 0)))
-                addFace(vertices, indices, position, Face.TOP)
+                addFace(vertexMap, verticesArrayList, indicesArrayList, position, Face.TOP)
 
             // Face Y Negative
             if (!filtered.containsKey(position + Vector3i(0, -1, 0)))
-                addFace(vertices, indices, position, Face.BOTTOM)
+                addFace(vertexMap, verticesArrayList, indicesArrayList, position, Face.BOTTOM)
 
             // Face Z Positive
             if (!filtered.containsKey(position + Vector3i(0, 0, 1)))
-                addFace(vertices, indices, position, Face.NORTH)
+                addFace(vertexMap, verticesArrayList, indicesArrayList, position, Face.NORTH)
 
             // Face Z Negative
             if (!filtered.containsKey(position + Vector3i(0, 0, -1)))
-                addFace(vertices, indices, position, Face.SOUTH)
+                addFace(vertexMap, verticesArrayList, indicesArrayList, position, Face.SOUTH)
         }
 
+        val vertices = BufferUtils.createFloatBuffer(verticesArrayList.size)
+        for (vertex in verticesArrayList)
+            vertices.put(vertex)
+        vertices.flip()
+
+        val indices = BufferUtils.createIntBuffer(indicesArrayList.size)
+        for (index in indicesArrayList)
+            indices.put(index)
+        indices.flip()
+
         return Pair(
-            vertices.flatMap { it.toList() }.toFloatArray(),
-            indices.toIntArray()
+            vertices,
+            indices
         )
     }
 
-    private fun setBufferData(vertices: FloatArray, indices: IntArray) {
-        indexCount = indices.size
+    private fun setBufferData(vertices: FloatBuffer, indices: IntBuffer) {
+        indexCount = indices.limit()
 
         glBindVertexArray(vao)
-
-        glBindBuffer(GL_ARRAY_BUFFER, vbo)
-        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
 
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.SIZE_BYTES, 0L * Float.SIZE_BYTES)
         glEnableVertexAttribArray(0)
 
         glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.SIZE_BYTES, 3L * Float.SIZE_BYTES)
         glEnableVertexAttribArray(1)
-//        glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
-//        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
 
         glBindVertexArray(0)
     }
 
     private fun addFace(
-        vertices: MutableList<Vertex>,
-        indices: MutableList<Int>,
+        vertexMap: MutableMap<Vertex, Int>,
+        vertices: ArrayList<Float>,
+        indices: ArrayList<Int>,
         position: Vector3i,
         face: Face
     ) {
         val v0 = Vertex(position + face.offset0, face.normal)
-        if (v0 !in vertices)
-            vertices.add(v0)
+        if (!vertexMap.containsKey(v0)) {
+            vertexMap[v0] = vertexMap.size
+            vertices.addAll(v0.toList())
+        }
 
         val v1 = Vertex(position + face.offset1, face.normal)
-        if (v1 !in vertices)
-            vertices.add(v1)
+        if (!vertexMap.containsKey(v1)) {
+            vertexMap[v1] = vertexMap.size
+            vertices.addAll(v1.toList())
+        }
 
         val v2 = Vertex(position + face.offset2, face.normal)
-        if (v2 !in vertices)
-            vertices.add(v2)
+        if (!vertexMap.containsKey(v2)) {
+            vertexMap[v2] = vertexMap.size
+            vertices.addAll(v2.toList())
+        }
 
         val v3 = Vertex(position + face.offset3, face.normal)
-        if (v3 !in vertices)
-            vertices.add(v3)
+        if (!vertexMap.containsKey(v3)) {
+            vertexMap[v3] = vertexMap.size
+            vertices.addAll(v3.toList())
+        }
 
-        indices.add(vertices.indexOf(v0))
-        indices.add(vertices.indexOf(v1))
-        indices.add(vertices.indexOf(v2))
+        indices.add(vertexMap[v0]!!)
+        indices.add(vertexMap[v1]!!)
+        indices.add(vertexMap[v2]!!)
 
-        indices.add(vertices.indexOf(v2))
-        indices.add(vertices.indexOf(v3))
-        indices.add(vertices.indexOf(v0))
+        indices.add(vertexMap[v2]!!)
+        indices.add(vertexMap[v3]!!)
+        indices.add(vertexMap[v0]!!)
     }
 
     fun render() {
