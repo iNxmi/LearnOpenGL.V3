5b190a1298a06290fa9a223c91d2f8053dcece3b
Memphis Herghelegiu <memphispc70@gmail.com>
2026-01-03 20:43:07 +0100

    backup

--- src/main/kotlin/com/nami/scene/scenes/MainMenuScene.kt
+++ src/main/kotlin/com/nami/scene/scenes/MainMenuScene.kt
@@ -5,11 +5,15 @@ import com.nami.scene.Scene
 import com.nami.scene.SceneManager
 import com.nami.world.World
 import imgui.ImGui
+import imgui.flag.ImGuiDataType
 import imgui.flag.ImGuiWindowFlags
+import imgui.type.ImLong
 import org.joml.Vector3i
 import org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose
 
 class MainMenuScene : Scene() {
+
+    val seed = ImLong(System.currentTimeMillis())
 
     override fun onRenderHUD() {
         ImGui.setNextWindowPos(0f, 0f)
@@ -18,8 +22,9 @@ class MainMenuScene : Scene() {
         ImGui.getFont().scale = 2.5f
         ImGui.begin("Main Menu", ImGuiWindowFlags.NoDecoration or ImGuiWindowFlags.NoMove)
 
+        ImGui.inputScalar("Seed", ImGuiDataType.S64, seed)
         if (ImGui.button("Play")) {
-            val world = World(Vector3i(512), System.currentTimeMillis(), 64)
+            val world = World(Vector3i(512), seed.get(), 64)
             SceneManager.set(PlayScene(world))
         }
 
